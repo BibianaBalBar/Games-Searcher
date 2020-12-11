6202e8ab8d24e192931fc86b5ff0c6d98af5a5c9
BibianaBalBar <balbuenabibi@gmail.com>
2020-12-11 21:12:28 +0000

    platforms icons

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -5,16 +5,44 @@ import styled from "styled-components";
 import { motion } from "framer-motion";
 import { useHistory } from "react-router-dom";
 import { smallImage } from "../util";
-
+//Images
+import playstation from "../img/playstation.svg";
+import steam from "../img/steam.svg";
+import xbox from "../img/xbox.svg";
+import nintendo from "../img/nintendo.svg";
+import apple from "../img/apple.svg";
+import gamepad from "../img/gamepad.svg";
+//Star Images
+import starEmpty from "../img/star-empty.png";
+import starFull from "../img/star-full.png";
 
 const GameDetail = ({pathId}) => {
   const history = useHistory();
+
   //Exit Detail
   const exitDetailHandler = (e) => {
     const element = e.target;
     if(element.classList.contains("shadow")){
       document.body.style.overflow = "auto";
       history.push("/")
+    }
+  };
+
+  //GET PLATFORM IMAGES
+  const getPlatform = (platform) => {
+    switch (platform) {
+      case "PlayStation 4":
+        return playstation;
+      case "Xbox One":
+        return xbox;
+      case "PC":
+        return steam;
+      case "Nintendo Switch":
+        return nintendo;
+      case "iOS":
+        return apple;
+      default:
+        return gamepad;
     }
   };
 
@@ -35,7 +63,11 @@ const GameDetail = ({pathId}) => {
                 <h3>Platforms</h3>
                 <Platforms>
                   {game.platforms.map(data => (
-                    <h3 key={data.platform.id}>{data.platform.name}</h3>
+                    <img 
+                      key={data.platform.id} 
+                      src={getPlatform(data.platform.name)} 
+                      alt={data.platform.name}
+                    />
                   ))}
                 </Platforms>
               </Info>
@@ -70,6 +102,7 @@ const CardShadow = styled(motion.div)`
   position: fixed;
   top: 0;
   left: 0;
+  z-index: 5;
   &::-webkit-scrollbar {
     width: 0.5rem;
   }
@@ -89,6 +122,7 @@ const Detail = styled(motion.div)`
   position: absolute;
   color: black;
   left: 10%;
+  z-index: 5;
   img {
     width: 100%;
   }
