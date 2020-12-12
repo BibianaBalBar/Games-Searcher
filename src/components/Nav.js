ebc501ac18c004e83f72dbbe8fdf8f9021b55e8b
BibianaBalBar <balbuenabibi@gmail.com>
2020-12-12 01:35:28 +0000

    fade and popup animations

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,9 +3,11 @@ import React, {useState} from "react";
 import styled from "styled-components";
 import { motion } from "framer-motion";
 import logo from "../img/logo.svg";
+import {fadeIn} from "../animations";
 //Redux and Routes
 import { fetchSearch } from "../actions/gamesAction";
 import { useDispatch } from "react-redux";
+
 
 const Nav = () => {
   const dispatch = useDispatch();
@@ -24,7 +26,7 @@ const Nav = () => {
   };
 
   return(
-    <StyledNav>
+    <StyledNav variants={fadeIn} initial="hidden" animate="show">
       <Logo onClick={clearSearched}>
         <img src={logo} alt="logo"/>
         <h1>Ignite</h1>
